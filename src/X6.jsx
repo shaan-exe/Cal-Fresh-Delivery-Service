3d2aab0643f66c96f5242a070a6a8c1212b18191
Shaan Makim <makimshaan8@gmail.com>
2025-07-01 20:32:09 -0700

    Initial commit

---
+++ src/X6.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+// import your JSON in here too!
+
+export default function School() {
+  return (
+    <main>
+      <h1>Hello world!</h1>
+      <p>Replace this JSX with your assignment.</p>
+    </main>
+  );
+}
+
+// more components down here!
---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Basic index.html file to load the React App
+// Don't mess with this!
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import School from "./X6.jsx";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <School />
+  </StrictMode>
+);
